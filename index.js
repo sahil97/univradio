99a742c454a901b33d133a3999a51fc693dc2a93
Sahil97 <sahil.sharma071997@gmail.com>
2019-02-25 13:59:50 +0530

    moved index.js file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var showSchema = new mongoose.Schema({
 var Show = mongoose.model('show', showSchema);
 // var Allshows;
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
